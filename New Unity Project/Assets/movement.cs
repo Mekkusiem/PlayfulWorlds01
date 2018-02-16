b8c2ffce27b9653235ec9ad496812f1247a87964
Mekkusiem <m.r.hilbrands@hotmail.com>
2018-02-16 11:07:30 +0100

    UnityProject01
    Project 01 eerste github shizz

---
+++ New Unity Project/Assets/movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z + 1);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 1, transform.eulerAngles.z);
+	}
+}
